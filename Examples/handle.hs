274c606056b5d053644afaf87a8490419c8840aa
Deian Stefan <deian@cs.stanford.edu>
2012-03-09 13:42:38 -0800

    use replicateM_ vs sequenceM_

--- Examples/handle.hs
+++ Examples/handle.hs
@@ -1,6 +1,7 @@
 module Main (main) where
 import MIO.Handle 
 import Mitigator
+import Control.Monad
 import Control.Monad.Trans
 import Control.Concurrent ( threadDelay )
 
@@ -8,11 +9,7 @@ import qualified Data.ByteString.Char8 as BS
 
 main = evalMitM $ do
   h <- openFile "/tmp/woz" ReadWriteMode
-  sequence_ $ replicate 5 $ hPut h (BS.pack "hello world")
+  replicateM_ 5 $ hPut h (BS.pack "hello world")
   lift $ putStrLn "about to miss" >> threadDelay 1900 >> putStrLn "DONE!"
-  sequence_ $ replicate 5 $ hPut h (BS.pack "hello world")
---  hPut h (BS.pack "hello world") >> hFlush h
---  sequence_ $ replicate 5 $ hPut h (BS.pack "hello world") >> hFlush h
---  lift $ putStrLn "starting read"
---  sequence_ $ replicate 15 $ hGet h 1 >>= \b -> lift $ BS.putStrLn b
+  replicateM_ 5 $ hPut h (BS.pack "hello world")
   hClose h
